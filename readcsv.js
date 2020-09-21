e5067c9fad1c92bb0ccb3a3b01b58b2bc0f83b6a
Michael Johnson <mrj0730@icloud.com>
2020-09-20 23:34:51 -0500

    add function to read all csv files in the directory

--- readcsv.js
+++ readcsv.js
@@ -2,18 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
 
-const FILES = ['ppt', 'facilityData'];
+const FILES = fs.readdirSync(path.join(__dirname, 'csv'), (err, files) => {
+  if (err) console.error(`ERROR: ${err}`);
+  return files;
+});
 
 function writeToJson(data, fileName) {
-  fs.writeFile(`./json/${fileName}.json`, data, (err) => {
+  let file = fileName.split('.')[0];
+
+  fs.writeFile(`./json/${file}.json`, data, (err) => {
     if (err) console.error(err);
-    else console.log(`Writing ${fileName}.json`);
+    else console.log(`Writing ${file}.json`);
   });
 }
 
 function updateAll(array) {
   for (let i = 0; i < array.length; i++) {
-    let file = path.resolve(__dirname, 'csv', `${array[i]}.csv`);
+    let file = path.resolve(__dirname, 'csv', array[i]);
     const results = [];
 
     fs.createReadStream(file)
