ab30657e8000c3ed34f674d25febee0a9a118231
Michael Johnson <mrj0730@icloud.com>
2020-09-21 21:54:52 -0500

    add schemas for all the mongo collections

--- connect.js
+++ connect.js
@@ -55,6 +55,85 @@ const paySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Pay = mongoose.model('Pay', paySchema);
+const facilitydataSchema = new mongoose.Schema(
+  {
+    facId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: String,
+    level: Number,
+    type: String,
+    address: String,
+    city: String,
+    state: String,
+    zip: String,
+    phone: String,
+    hours: String,
+    travelDays: Number,
+    lat: String,
+    long: String,
+  },
+  { collection: 'facilitydata' },
+  { timestamps: true }
+);
 
-module.exports = { db, Pay };
+const pptdataSchema = new mongoose.Schema(
+  {
+    'Service Area': String,
+    District: String,
+    'Facility Type': String,
+    Level: Number,
+    'Facility ID': String,
+    'Facility Name': String,
+    'CPC Target': Number,
+    'Current # of CPC On-Board': Number,
+    'CPC on Temp': Number,
+    'CPC on Long Term Hold': Number,
+    'Current # of CPC On-Board minus Temps minus LTH': Number,
+    'Current % CPC to Target': String,
+    'Training Time Years': Number,
+    'ATCS in Training': Number,
+    'ATCS in Training on Long Term Hold': Number,
+    'ATCS in Training minus LTH': Number,
+    'Training Success Rate': String,
+    'Current ATCS in Training Expected to Cert': Number,
+    'Committed ATCS Inbound': Number,
+    'Placement List Inbounds': Number,
+    'Temps Inbound': Number,
+    'Inbounds Expected to Cert + Temps Inbound': Number,
+    'Committed ATCS Outbound': Number,
+    'Placement List Outbounds': Number,
+    'Temps Outbound': Number,
+    'Projected Retirements and Other Losses': Number,
+    'Projected % to Target': String,
+    'Possible Gains to National Average': Number,
+    'Possible Gains to Target': Number,
+    'ERR Category': String,
+    'Possible Losses': Number,
+    'Current % CPC to Trainees': String,
+    TNE: Number,
+    AG: Number,
+    Dev1: Number,
+    Dev2: Number,
+    Dev3: Number,
+    'Dev Total': Number,
+    CPCIT0: Number,
+    CPCIT1: Number,
+    CPCIT2: Number,
+    CPCIT3: Number,
+    'CPCIT Total': Number,
+    HRRegion: String,
+    CoreAirport: String,
+    Type: Number,
+  },
+  { collection: 'pptdata' },
+  { timestamps: true }
+);
+
+const Fac = mongoose.model('Facilitydata', facilitydataSchema);
+const Pay = mongoose.model('Pay', paySchema);
+const Ppt = mongoose.model('Pptdata', pptdataSchema);
+
+module.exports = { db, Fac, Pay, Ppt };
